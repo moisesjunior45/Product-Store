d9a3ac53520d46c4cf1c5b2e566dd6ad9cdf2b2d
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2025-02-18 22:14:48 -0300

    refactor: refatora listagem de produto criando um componente para renderizar o mesmo

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,11 +4,12 @@ import { MatButtonModule} from '@angular/material/button';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })

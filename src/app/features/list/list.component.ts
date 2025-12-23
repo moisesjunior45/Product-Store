0c938899056a83c7152a3aa42b4f6826a1144ecf
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-12-22 22:14:48 -0300

    refactor: cria ConfirmationDialogService especializado na modal de confirmação

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,49 +1,11 @@
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MatDialog,
-  MatDialogModule,
-  MatDialogRef,
-} from '@angular/material/dialog';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
-
-@Component({
-  selector: 'app-confirmation-dialog',
-  template: `
-    <h2 mat-dialog-title>Deletar produto</h2>
-    <mat-dialog-content>
-      Tem certeza que quer deletar este produto?
-    </mat-dialog-content>
-    <mat-dialog-actions align="end">
-      <button mat-button (click)="onNo()">Não</button>
-      <button
-        mat-raised-button
-        color="accent"
-        (click)="onYes()"
-        cdkFocusInitial
-      >
-        Sim
-      </button>
-    </mat-dialog-actions>
-  `,
-  standalone: true,
-  imports: [MatButtonModule, MatDialogModule],
-})
-export class ConfirmationDialogComponent {
-  matDialogRef = inject(MatDialogRef);
-
-  onNo() {
-    this.matDialogRef.close();
-  }
-
-  onYes() {
-    this.matDialogRef.close(true);
-  }
-}
+import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 
 @Component({
   selector: 'app-list',
@@ -57,7 +19,7 @@ export class ListComponent {
 
   productsService = inject(ProductsService);
   router = inject(Router);
-  matDialog = inject(MatDialog);
+  confirmationDialogService = inject(ConfirmationDialogService);
 
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
@@ -70,9 +32,8 @@ export class ListComponent {
   }
 
   onDelete(product: Product) {
-    this.matDialog
-      .open(ConfirmationDialogComponent)
-      .afterClosed()
+    this.confirmationDialogService
+      .openDialog()
       .pipe(filter((answer) => answer === true))
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {

343976fc8ecadf8d696767a0acb661b883fe03f9
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-12-22 22:27:19 -0300

    refactor: melhora código de ListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,18 @@ import {
   Routes,
 } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { CreateComponent } from './features/create/create.component';
-import { Observable } from 'rxjs';
 import { inject } from '@angular/core';
 import { ProductsService } from './shared/services/products.service';
 
 export const routes: Routes = [
   {
     path: '',
+    resolve: {
+      products: () => {
+        const productsService = inject(ProductsService);
+        return productsService.getAll();
+      },
+    },
     component: ListComponent,
   },
   {
